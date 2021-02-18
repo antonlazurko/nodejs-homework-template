c20836bb7e47b41cc923bd1bbc5ca9420eab4356
unknown <a.lazurko.al@gmail.com>
2021-02-18 14:53:30 +0200

    делает лист и контакт с шаблонами

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 const contactsRouter = require('./routes/api/contacts');
 
 const app = express();
+app.set('views', path.normalize('./views'));
+app.set('view engine', 'ejs');
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(express.json());
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use('/', contactsRouter);
 
-app.use((req, res) => {
+app.use((_req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   res.status(500).json({ message: err.message });
 });
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -9,24 +9,42 @@ const {
   addContact,
 } = require('../../services/services.js');
 
+// router.get('/', async (req, res, next) => {
+//   try {
+//     let data = await listContacts();
+//     res.status(200).json({ message: data });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
+
 router.get('/', async (req, res, next) => {
   try {
     let data = await listContacts();
-    res.status(200).json({ message: data });
+    res.render('contacts', {
+      title: 'Contacts',
+      contacts: data,
+    });
   } catch (e) {
+    console.log(e);
     next(e);
   }
 });
 
-router.get(`/:contactId`, async (req, res, next) => {
+router.get('/:contactId', async (req, res, next) => {
   const { pathname } = url.parse(req.url);
   let contactId = pathname.substring(1);
   try {
     let data = await getContactById(contactId);
-    res.status(200).json({ message: data });
+    console.log(data);
+    if (data === {}) {
+      res.render('contact', {
+        title: 'Contact',
+        contact: data,
+      });
+    }
+    next();
   } catch (e) {
-    res.status(404);
-    console.log(e);
     next(e);
   }
 });
